057e38936d65a062055ee3f385b6ecb85c9d95f6
Kazuma Kusu <kusu@mil.doshisha.ac.jp>
2025-05-12 06:04:46 +0900

    refactor: Update footer

--- app/app.config.ts
+++ app/app.config.ts
@@ -37,25 +37,15 @@ export default defineAppConfig({
     credits: `Copyright © ${new Date().getFullYear()}`,
     colorMode: false,
     links: [{
-      'icon': 'i-simple-icons-nuxtdotjs',
-      'to': 'https://nuxt.com',
-      'target': '_blank',
-      'aria-label': 'Nuxt Website'
+      icon: 'i-simple-icons-x',
+      to: 'https://x.com/camphoratech',
+      target: '_blank',
+      'aria-label': 'CamphoraTech on X'
     }, {
-      'icon': 'i-simple-icons-discord',
-      'to': 'https://discord.com/invite/ps2h6QT',
-      'target': '_blank',
-      'aria-label': 'Nuxt UI on Discord'
-    }, {
-      'icon': 'i-simple-icons-x',
-      'to': 'https://x.com/nuxt_js',
-      'target': '_blank',
-      'aria-label': 'Nuxt on X'
-    }, {
-      'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/nuxt/ui',
-      'target': '_blank',
-      'aria-label': 'Nuxt UI on GitHub'
+      icon: 'i-simple-icons-github',
+      to: 'https://github.com/camphora-tech',
+      target: '_blank',
+      'aria-label': 'CamphoraTech on GitHub'
     }]
   },
   toc: {
