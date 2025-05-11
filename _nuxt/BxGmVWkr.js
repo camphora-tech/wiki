24d83de040d37c4e47fad197f371646e4abc303c
xkazuma <kusu@mil.doshisha.ac.jp>
2025-05-11 20:11:10 +0000

    Deploying to gh-pages from @ camphora-tech/wiki@5794b206be8721927c7ed8cd9153229512ce7a9b 🚀

---
+++ _nuxt/BxGmVWkr.js
@@ -0,0 +1,1 @@
+import{r as n}from"./D-VdKrQ7.js";import a from"./DIVtVFIG.js";import{z as s,H as t,i as e,w as i,W as p}from"./97GXWPbJ.js";import"./C2bHYaEW.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
