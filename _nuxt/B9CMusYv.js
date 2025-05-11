f162ae2c9c0684ac2ba93dfe804e5fb4829b3901
xkazuma <kusu@mil.doshisha.ac.jp>
2025-05-11 19:55:40 +0000

    Deploying to gh-pages from @ camphora-tech/wiki@4f1b2bbe442d06283dd7070975dd4ed0099ed318 🚀

---
+++ _nuxt/B9CMusYv.js
@@ -0,0 +1,1 @@
+import{r as n}from"./CpkGmk-Y.js";import a from"./jYHqBRrF.js";import{z as s,H as t,i as e,w as i,W as p}from"./CsRTRT4z.js";import"./C2bHYaEW.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
---
+++ _nuxt/BGYQseSW.js
@@ -0,0 +1,1 @@
+import{r as t}from"./CpkGmk-Y.js";import a from"./jYHqBRrF.js";import{z as e,H as n,i as s,w as i,W as c}from"./CsRTRT4z.js";import"./C2bHYaEW.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:c(o).ui.icons.caution},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
