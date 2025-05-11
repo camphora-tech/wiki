f73236b17d7d6f779c50945bffb3ea664dc8f9d9
xkazuma <kusu@mil.doshisha.ac.jp>
2025-05-11 21:05:57 +0000

    Deploying to gh-pages from @ camphora-tech/wiki@057e38936d65a062055ee3f385b6ecb85c9d95f6 🚀

---
+++ _nuxt/BqIUy-AT.js
@@ -0,0 +1,1 @@
+import{r as t}from"./Dg-46bCR.js";import e from"./0uP_O7R-.js";import{z as n,H as s,i as a,w as i,W as p}from"./DeqYr5Pw.js";import"./C2bHYaEW.js";const d={__name:"ProseNote",setup(c){const o=n();return(r,f)=>(a(),s(e,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
