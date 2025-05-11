f90c56ef08dfdf0afe32c30a92ccec8bf784428a
xkazuma <kusu@mil.doshisha.ac.jp>
2025-05-11 20:23:58 +0000

    Deploying to gh-pages from @ camphora-tech/wiki@3189fc6f2318a8acb9514e89221288bca7e5793d 🚀

---
+++ _nuxt/Bq00at7G.js
@@ -0,0 +1,1 @@
+import{r as s}from"./BmHbHYm0.js";import t from"./BjfYc3Uw.js";import{z as e,H as a,i as p,w as c,W as i}from"./BjpXoZDF.js";import"./C2bHYaEW.js";const d={__name:"ProseTip",setup(n){const o=e();return(r,f)=>(p(),a(t,{color:"success",icon:i(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
